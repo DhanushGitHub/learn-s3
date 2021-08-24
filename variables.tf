9123b2aa1fbf6fe366b6c761771ed1c81d8baede
DhanushGitHub <89379527+DhanushGitHub@users.noreply.github.com>
2021-08-24 09:45:33 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "s3_bucket_name" {
+  description = "s3 bucket name"
+  type        = string
+  default     = "Dhanush_s3"
+}
+
+variable "s3_env" {
+  description = "environment for s3"
+  type        = string
+  default     = "Dhanush_DEV_s3"
+}
